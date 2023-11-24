1c3d813fc5be41e6958897df3b83d78d6709cbf3
Никита Гудим <nikita400g@gmail.com>
2023-11-25 02:52:32 +0700

    сменил форум на 4pda ибо все остальные форумы или были написаны через 5 точку или были некультурными. готовая программа

--- PikabuParser/Program.cs
+++ PikabuParser/Program.cs
@@ -9,6 +9,7 @@ using DBWork;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using System.Threading;
+using System.Windows.Markup;
 
 namespace PikabuParser
 {
@@ -16,17 +17,26 @@ namespace PikabuParser
     {
         static void Main()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Url = "https://www.playground.ru/misc/opinion/top_25_anime_o_gejmerah_i_dlya_gejmerov_hudshee_luchshee_i_alternativy_chast_1-1662933";
-            IList<IWebElement> names = driver.FindElements(By.ClassName("comments-item-author"));
-            IList<IWebElement> texts = driver.FindElements(By.ClassName("comments-item-content"));
-            IList<IWebElement> tboxes = driver.FindElements(By.ClassName("comments-item"));
-            List<string> ids = new List<string>();
-            foreach (IWebElement element in tboxes)
+            IWebDriver driver = new ChromeDriver(); //создаем драйвер
+            driver.Url = "https://4pda.to/forum/index.php?showtopic=1056193";//задаем сылку на страницу форума
+            IList<IWebElement> names = driver.FindElements(By.ClassName("normalname"));//находим все IWebElement с наименованием класса normalname
+            IList<IWebElement> texts = driver.FindElements(By.ClassName("postcolor"));//находим все IWebElement с наименованием класса postcolor
+            IList<IWebElement> idblocks = driver.FindElements(By.ClassName("ipbtable"));//находим все IWebElement с наименованием класса ipbtable
+            IList<IWebElement> updatedList = idblocks.Skip(1).ToList();//создаем новый список с ipbtable но без одного лишнего элемента
+            List<int> ids = new List<int>();//будущий список с id комментариев
+            foreach (IWebElement idblock in updatedList)//для каждого элемента с названием класса ipbtable
             {
-                ids.Add(element.GetAttribute("data-comment-id").ToString());
+                ids.Add(Convert.ToInt32(idblock.GetAttribute("data-post")));//забираем значение атрибута data-post и закидываем его в List
             }
-            Console.WriteLine(names[0].Text);
+            Console.WriteLine(names[1].Text);//I_M_H_O
+            Console.WriteLine(texts[1].Text);//приложение устанавливается без ярлыка
+            Console.WriteLine(ids[1]);//117958126
+
+            //Class1 class1 = new Class1();//создание экземпляра класса для работы с бд
+            //for (int i = 1; i < names.Count; i++)//запускаем массив
+            //{
+            //    class1.Add(ids[i], names[i].Text, texts[i].Text);//вставляем все значения по очереди
+            //}
         }
     }
 }
